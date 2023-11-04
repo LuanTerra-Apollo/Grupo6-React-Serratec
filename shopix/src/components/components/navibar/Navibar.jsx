5bc34bbd6f92197f2c149312f020c28bc0a0e842
brunazimbrao <bruna.z.silva@aluno.senai.br>
2023-11-03 22:21:11 -0300

    navibar completo

--- shopix/src/components/components/navibar/Navibar.jsx
+++ shopix/src/components/components/navibar/Navibar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import boneco from '../../../img/boneco.png'
 import styled from 'styled-components';
 import carrinho from '../../../img/carrinho.png'
+import { InputBarraPesquisa } from '../../styles/Inputs.style';
 
 
 const Navi = styled.nav`
@@ -17,12 +18,49 @@ const Navi = styled.nav`
         width: 100%;
         height: 100%;
     }
-`;
+
+    @media  (max-width: 854px){
+
+        .Pesquisa{
+            width:400px;
+            height: 30px;
+        }
+
+        .EntraRegitrar{
+            font-size: 10px;
+        }
+
+        .DireitaNav{
+            padding-right: 30px;
+
+        }
+        .User{
+            width: 20px;
+            height: 20px;
+        }
+        .Car{
+            width: 25px;
+            height: 25px;
+        }
+    }
+        
+    
+`
+
 
 const Marca = styled.div`
   
     width: 200px;
     height: 45px;
+
+    .Marca{
+        
+        &:hover{
+            -webkit-filter: drop-shadow(5px 5px 5px #df6fda);
+             filter: drop-shadow(6px 4px 4px #df6fda);
+        
+        }
+    }
     
 
 `
@@ -30,12 +68,17 @@ const Car = styled.div`
     width: 30px;
     height: 30px;
     margin: 20px;
+
+    &:hover{
+        -webkit-filter: drop-shadow(5px 5px 5px #df6fda);
+        filter: drop-shadow(6px 4px 4px #df6fda);
+    }
  
 `
-const User =styled.div `
+const User = styled.div`
     width: 30px;
     height: 30px;
-    margin-right:5px;
+    margin-right:10px;
 
 
 `
@@ -43,30 +86,44 @@ const DireitaNav = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding-right: 65px;
+
+.EntrarRegistrar{
+        color:white;
+        text-decoration: none;
+
+
+&:hover{
+        color:#df6fda ;
+        -webkit-filter: drop-shadow(5px 5px 5px #df6fda);
+        filter: drop-shadow(6px 4px 4px #df6fda);
+            }
+    }
 `
 
 
-const  Navibar = () => {
-    return(
+const Navibar = () => {
+    return (
         <Navi >
             <Marca>
-                <img src={logo} />
+                <Link className='Marca' to ='/'><img src={logo} /></Link>
             </Marca>
-            
+
             <div>
-                <input type="text" />
+                <InputBarraPesquisa className='Pesquisa' type="text" placeholder='pesquisar' />
             </div>
-            
-            <DireitaNav>
-                <User>
+
+            <DireitaNav className='DireitaNav'>
+                <User className='User'>
                     <img src={boneco} alt="" />
                 </User>
-                <div>
-                    <Link to = '/login' style={{color:'white',textDecoration:'none'}}>Entre/Registrar</Link> 
-                </div>
 
-                <Car>
-                    <Link to = '/carrinho'>  <img src={carrinho}alt="" /></Link>
+
+                <Link className='EntrarRegistrar' to='/login'>Entre/Registrar</Link>
+
+
+                <Car className='Car'>
+                    <Link to='/carrinho'>  <img src={carrinho} alt="" /></Link>
                 </Car>
             </DireitaNav>
         </Navi>
