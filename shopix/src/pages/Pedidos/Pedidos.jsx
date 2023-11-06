c7e7c4e1e07b5b1136f07a173d83a3b0605d3dec
LuanTerra-Apollo <luan_terra12@hotmail.com>
2023-11-06 15:25:13 -0300

    Lógicas para Pedido

--- shopix/src/pages/Pedidos/Pedidos.jsx
+++ shopix/src/pages/Pedidos/Pedidos.jsx
@@ -6,31 +6,53 @@ import { Wrapper } from "../../components/styles/Wrapper.style"
 import { api } from "../../api/api"
 
 const Pedidos = () => {
-    const [pedidos, setPedidos] = useState({})
+    const [pedidos, setPedidos] = useState([])
+    const [produtosPedido, setProdutosPedido] = useState([])
 
     const userId = JSON.parse(localStorage.getItem('user_id')).id;
 
     useEffect(() => {
         handleCarregarPedidos()
+        handleCarregarProdutosPedido()
     }, [])
 
     const handleCarregarPedidos = async () => {
         const response = await api.get('/pedidos')
 
-        const pedidosDoUsuario = []
-        response.data.forEach((pedido) => {
+        
+        const pedidosDoUsuario = response.data.map((pedido) => {
             if (pedido.idUser === userId){
-                pedidosDoUsuario.push(pedido)
+                return pedido
             }
         })
 
-    setPedidos(pedidosDoUsuario)
+        console.log(pedidosDoUsuario)
+
+        setPedidos(pedidosDoUsuario)
+
+        console.log(pedidos)
+    }
+
+    const handleCarregarProdutosPedido = async () => {
+        const response = await api.get('/produtos')
+
+        const produtosDoPedido = response.data.map((produto) => {
+            if (pedidos.forEach(({item}) => item.idProduto == produto.id)) {
+                return produto
+            }
+        })
+
+        console.log(produtosDoPedido)
+        
+        setProdutosPedido(produtosDoPedido)
+
+        console.log(produtosPedido)
     }
 
     return (
         <Wrapper>
             <Navibar/>
-                <ListaPedidos pedidos={pedidos} />
+                <ListaPedidos pedidos={pedidos} produtosPedido={produtosPedido} />
             <Footer/>            
         </Wrapper>
     )
