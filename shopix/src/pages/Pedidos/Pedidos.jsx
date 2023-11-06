6348212745b5fe6355d1a8736adb22560c7504e1
LuanTerra-Apollo <luan_terra12@hotmail.com>
2023-11-06 03:57:47 -0300

    Começando a lógica de pedidos

--- shopix/src/pages/Pedidos/Pedidos.jsx
+++ shopix/src/pages/Pedidos/Pedidos.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react"
 import ListaPedidos from "../../components/components/ListaPedidos"
+import Footer from "../../components/components/footer/Footer"
+import Navibar from "../../components/components/navibar/Navibar"
 import { Wrapper } from "../../components/styles/Wrapper.style"
+import { api } from "../../api/api"
 
 const Pedidos = () => {
+    const [pedidos, setPedidos] = useState({})
+
+    const userId = JSON.parse(localStorage.getItem('user_id')).id;
+
+    useEffect(() => {
+        handleCarregarPedidos()
+    }, [])
+
+    const handleCarregarPedidos = async () => {
+        const response = api.get('/pedidos', {params: {idUser: userId}})
+
+    setPedidos(response.data)
+    }
+
+    console.log(pedidos)
+
+
+
     return (
         <Wrapper>
-            <ListaPedidos />            
+            <Navibar/>
+                <ListaPedidos pedidos={pedidos} />
+            <Footer/>            
         </Wrapper>
     )
 }
